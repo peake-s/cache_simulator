d1f3b554ca901d2cce15a8e562740a72562b6b27
Sammy Peake <peakes@vcu.edu>
2022-11-23 11:14:13 -0500

    stuff

--- cache.py
+++ cache.py
@@ -1,7 +1,9 @@
 import bitstring as bs 
 from bitstring import BitArray as ba
 from math import log
-
+import sys
+import argparse
+import pandas as pd
 class cache:
     def __init__(self,block_size=16,num_blocks=16,associativity = 1):
         #2d cache data structure with all rows init to 0's
@@ -16,6 +18,7 @@ class cache:
         self.address_size = 32
         self._addr_structure(block_size,num_blocks,associativity)
         self.cache = self._create_cache(num_blocks,32)
+        self.size = num_blocks * block_size
 
     def _addr_structure(self,block_size,num_blocks,associativity=1):
         num_sets = 0
@@ -38,23 +41,47 @@ class cache:
         return [bs.BitArray(row_len) for i in range(num_blocks)]
         
 class direct_mapping:
-    def __init__(self):
+    def __init__(self,cache):
+        self.cache = cache
+        self.hits = 0
+        self.misses = 0
+        self.num_reads = 0
+
+    def cache_read(self):
+        pass
+
+    def check_valid(self):
+        pass
+
+    def check_tag(self):
         pass
 
 class associative_mapping:
     def __init__(self):
         pass
 
-def read_addr_file():
-    pass
+def read_addr_file(file):
+    addr = pd.read_csv(file, delimiter=' ', header=None)
+    return addr[0].values.tolist()
 
-def read_input():
-    pass
-
-def main():
-    c = cache(16,64)
-    print(f"idx: {c.index_bits} tag: {c.tag_bits} off: {c.offset}")
-    print(c.cache[0])   
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--input_file")
+    parser.add_argument("-b","--block_size")
+    parser.add_argument("-n","--num_blocks")
+    parser.add_argument("-a", "--associativity")
+    parser.add_argument("-t","--hit_time")
+    parser.add_argument("-m","--miss_time")
+    parser.add_argument("-l","--lru")
+    args = parser.parse_args()
+    address_data = read_addr_file(args.input_file)
+    block_size = args.block_size
+    num_block = args.num_blocks
+    associativity = args.associativity
+    hit_time = args.hit_time
+    miss_time = args.miss_time
+    lru = args.lru
+    cche = cache(block_size=block_size,num_blocks=num_block,associativity=1)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
